152f53bf00ad8a52756bc5b4a7c7c2be72fcb6e6
Barbara Fick <bkantzos@yahoo.com>
2019-02-27 11:57:22 -0700

    temporarily using ngIf to navigate between shopping and recipes until routing is enabled.  also passing recipe from recipe list to recipe item and seperating components more properly

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
     selector: 'app-header',
@@ -7,5 +7,10 @@ import { Component } from "@angular/core";
 }
 )
 export class HeaderComponent {
+    @Output() selectedFeature = new EventEmitter<String>();
 
+    onSelect(feature: string) {
+        console.log(feature);
+        this.selectedFeature.emit(feature);
+    }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -8,8 +8,8 @@ import { Ingredient } from '../shared/ingredient.model';
 })
 export class ShoppingListComponent implements OnInit {
   ingredients: Ingredient[] = [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10),
+    new Ingredient('Chicken Breasts', 4),
+    new Ingredient('Potatoes', 4),
   ]
 
   constructor() { }
